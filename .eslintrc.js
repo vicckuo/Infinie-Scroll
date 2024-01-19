d94c5dde98affde2826be6ee7e809813538618db
Vic Kuo <eun.hru@gmail.com>
2024-01-19 15:51:22 +0800

    feat: 完成無限滾動功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     quotes: "off",
+    semi: "off",
+    "comma-dangle": 0,
   },
 };
--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,7 +1,58 @@
 <template>
-  <div id="infinite-scroll">scroll</div>
+  <div id="infinite-scroll">
+    <ol>
+      <li v-for="item in items" :key="item.id">{{ item.title }}</li>
+    </ol>
+
+    <div v-if="loading">讀取中...</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import axios from "axios";
+
+const items = ref([]);
+const loading = ref(false);
+const itemCount = ref(1);
+
+const loadMore = async () => {
+  if (loading.value) return;
+
+  loading.value = true;
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/octocat/Spoon-Knife/issues?page=${itemCount.value}`
+    );
+    if (itemCount.value === 1) {
+      items.value = items.value.concat(response.data);
+      itemCount.value += 1;
+    } else {
+      items.value = items.value.concat(response.data.slice(0, 10));
+      itemCount.value += 1;
+    }
+  } catch (error) {
+    console.error("GET API ERROR", error);
+  }
+  loading.value = false;
+};
+
+const handleScroll = () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (clientHeight + scrollTop >= scrollHeight - 5) {
+    loadMore();
+  }
+};
+
+onMounted(() => {
+  loadMore();
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
 
 <style lang="scss" scoped></style>
